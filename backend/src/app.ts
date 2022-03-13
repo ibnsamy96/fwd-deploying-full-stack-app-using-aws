5b0e0c190c08610b895002b6ddee9befec7cde07
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-03-13 17:21:28 +0200

    fixed dotenv not found issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,30 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
-import db from './database'
+import express, { Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import db from "./database";
 
-dotenv.config()
+dotenv.config();
 
-const app: Application = express()
-const port = process.env.PORT || 80
+const app: Application = express();
+const port = process.env.PORT || 80;
 
-app.use(
-    cors(),
-    morgan('dev'),
-    express.json(),
-)
+app.use(cors(), morgan("dev"), express.json());
 
-app.get('/', (req, res) => {
-    res.send(`Use './quotes' endpoint with GET method to get the available quotes!`);
+app.get("/", (req, res) => {
+  res.send(
+    `Use './quotes' endpoint with GET method to get the available quotes!`
+  );
 });
 
-app.get('/quotes', async (req, res) => {
-    
-    const availableQuotes = await db.query('SELECT text, author FROM quotes');
-    res.send(availableQuotes.rows).status(200)
+app.get("/quotes", async (req, res) => {
+  const availableQuotes = await db.query("SELECT text, author FROM quotes");
+  res.send(availableQuotes.rows).status(200);
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}.`)
-})
+  console.log(`Server started on port ${port}.`);
+});
 
-export default app
+export default app;
