7e21fba47fd8719a8aeb85712b2949ed96a4333c
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-03-13 15:28:37 +0200

    fixed a command

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,4 +1,4 @@
 eb init --platform node.js --region us-east-1
 eb use fwd-proj-backend
-eb deploy backend
+eb deploy fwd-proj-backend
 eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
