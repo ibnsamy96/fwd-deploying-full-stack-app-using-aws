e286c0b47105aa398ffa26ad42de9baacd50b159
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-03-13 17:07:38 +0200

    added simple updates for the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,29 +6,28 @@ import db from './database'
 
 dotenv.config()
 
-const app: Application = express() // create an express application
-const port = process.env.PORT || 80 // set the port
+const app: Application = express()
+const port = process.env.PORT || 80
 
-// Defaul middlewares
 app.use(
-    cors(), // enable cors
-    morgan('dev'), // log requests to the console
-    express.json(), // parse application/json
+    cors(),
+    morgan('dev'),
+    express.json(),
 )
 
 app.get('/', (req, res) => {
-    res.send('The API is working!');
+    res.send(`Use './quotes' endpoint with GET method to get the available quotes!`);
 });
 
 app.get('/quotes', async (req, res) => {
     
-    const quotes = await db.query('SELECT text, author FROM quotes');
-    res.send(quotes.rows).status(200)
+    const availableQuotes = await db.query('SELECT text, author FROM quotes');
+    res.send(availableQuotes.rows).status(200)
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}.`)
 })
 
 export default app
