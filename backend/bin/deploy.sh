c9df7014bc518a890b3336a9f8caf02e57e50413
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-03-13 15:27:26 +0200

    added deployment commands

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,2 +1,4 @@
+eb init --platform node.js --region us-east-1
+eb use fwd-proj-backend
 eb deploy backend
 eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
