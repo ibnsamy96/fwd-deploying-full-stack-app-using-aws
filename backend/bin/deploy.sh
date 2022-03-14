1a1e1659f39467dd3c8c16f2b806886f43245a55
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-03-14 15:52:28 +0200

    commenting some code

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,4 +1,3 @@
 eb init --platform node.js --region us-east-1
 eb use fwd-proj-backend
-eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
 eb deploy fwd-proj-backend
